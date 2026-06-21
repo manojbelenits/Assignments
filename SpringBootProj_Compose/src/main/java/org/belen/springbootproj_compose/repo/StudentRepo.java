5654924fe74c470c15f7aaf0689ef5ce1026b249
mmanoj <mmunnoru@belenits.in>
2026-06-21 08:26:13 +0530

    clean

--- SpringBootProj_Compose/src/main/java/org/belen/springbootproj_compose/repo/StudentRepo.java
+++
@@ -1,7 +0,0 @@
-package org.belen.springbootproj_compose.repo;
-
-import org.belen.springbootproj_compose.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepo  extends JpaRepository<Student,Integer> {
-}
